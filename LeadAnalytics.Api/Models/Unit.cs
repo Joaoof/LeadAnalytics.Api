a1a8d2510e07f3d0f9a9903212f611d71cc2fc74
Joao de Deus <joaodeus400@gmail.com>
2026-03-28 11:33:03 -0300

    feat: adiciona model unit para cada unidade

---
+++ LeadAnalytics.Api/Models/Unit.cs
@@ -0,0 +1,9 @@
+﻿namespace LeadAnalytics.Api.Models;
+
+public class Unit
+{
+    public int Id { get; set; }
+    public int ClinicId { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
