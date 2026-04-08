fb56290a1a4a0900a165fb9198afe7f6349335bc
Joaoof <joaodeus400@gmail.com>
2026-04-07 21:03:45 -0300

    feat: adiciona json name customer que é o id do usuário

--- LeadAnalytics.Api/DTOs/SyncLeadDto.cs
+++ LeadAnalytics.Api/DTOs/SyncLeadDto.cs
@@ -4,19 +4,26 @@ namespace LeadAnalytics.Api.DTOs;
 
 public class SyncLeadDto
 {
+    [JsonPropertyName("customer")]
     public int ExternalId { get; set; }
     [JsonPropertyName("tags")]
     public List<string> Tags { get; set; } = [];
+
     [JsonPropertyName("name")]
     public string? Name { get; set; }
-    [JsonPropertyName("Phone")]
+
+    [JsonPropertyName("phone")]
     public string? Phone { get; set; }
+
     [JsonPropertyName("stage_label")]
     public string? Stage { get; set; }
-    [JsonPropertyName("clinic_id")]
+
+    [JsonPropertyName("idclinic")]
     public int TenantId { get; set; }
-    [JsonPropertyName("creation_date")]
+
+    [JsonPropertyName("created_at")]
     public DateTime? CreatedAt { get; set; }
+
     [JsonPropertyName("updated_at")]
     public DateTime? UpdatedAt { get; set; }
 }
